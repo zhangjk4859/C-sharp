7b4fbd43cfd30d4422f6ca52bfece69f275c3a59
zhangjk4859 <573581325@qq.com>
2017-02-06 11:33:48 +0800

    no message

--- 心连心登录/心连心登录/operateForm.Designer.cs
+++ 心连心登录/心连心登录/operateForm.Designer.cs
@@ -92,6 +92,7 @@
             this.button2.TabIndex = 4;
             this.button2.Text = "点击导出excel";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // textBox2
             // 
